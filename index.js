26b151d9b9eb17f6b74c6ae2ba3688a54efae8e4
trhacknonimous <82779890+trhacknonimous@users.noreply.github.com>
2023-08-11 23:33:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,217 @@
+const express = require('express');
+const helmet = require('helmet');
+const xss = require('xss');
+const { JSDOM } = require('jsdom');
+const { window } = new JSDOM('');
+const dompurify = require('dompurify')(window);
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(helmet()); // Utilisation du middleware Helmet pour ajouter des en-têtes de sécurité
+const port = 3000;
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const files = getMusicFiles();
+    const rapContent = getRapContent();
+    res.send(buildHTML(files, rapContent));
+});
+
+// Utilisation de xss pour filtrer les entrées utilisateur potentiellement dangereuses
+function sanitizeInput(input) {
+    return xss(input);
+}
+
+// Utilisation de dompurify pour nettoyer les données HTML potentiellement dangereuses
+function sanitizeHTML(html) {
+    return dompurify.sanitize(html);
+}
+
+
+function getMusicFiles() {
+    const files = fs.readdirSync(path.join(__dirname, 'public'));
+    return files.filter(file => ['.mp3', '.webm', '.mp4'].includes(path.extname(file)));
+}
+
+function getRapContent() {
+    return [
+        { artist: 'TAFTAF', song: 'BAZAR', punchline: 'on parle pas aux bavard.' },
+        { artist: 'Zbig', song: 'DlD', punchline: 'que du sale ma dis le four...arrete un peu de taffole . tous on le sait que test une follete' },
+        { artist: 'voleur2voleur', song: 'INDÉCIS', punchline: 'devant les calibres sa devient sentimental.' },
+        { artist: 'RAOUS', song: 'terrain mine', punchline: `Sur le terrain, j'pose ma mine, mon rap est explosif, t'entends le gyrophare briller comme une étoile dans la nuit.` },
+        // Ajoutez plus de contenu ici
+    ];
+}
+
+function buildHTML(files, rapContent) {
+    let listHTML = '';
+    let rapContentHTML = '';
+
+    for (const file of files) {
+        const fileType = path.extname(file).toLowerCase();
+        let fileLink = `/play?file=${encodeURIComponent(file)}`;
+        if (fileType === '.mp4') {
+            listHTML += `
+                <li>
+                    <video width="320" height="240" controls>
+                        <source src="${fileLink}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                </li>`;
+        } else if (fileType === '.mp3') {
+            listHTML += `
+                <li>
+                    <audio controls>
+                        <source src="${fileLink}" type="audio/mpeg">
+                        Your browser does not support the audio tag.
+                    </audio>
+                </li>`;
+        }
+    }
+
+    for (const rap of rapContent) {
+        rapContentHTML += `
+            <div class="rap">
+                <h2>${rap.artist} - ${rap.song}</h2>
+                <p>${rap.punchline}</p>
+            </div>
+        `;
+    }
+   return `
+        <!DOCTYPE html>
+        <html lang="fr"> <!-- Indiquez la langue de votre site -->
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Rap Attack Player</title>
+            <link rel="icon" href="hack.ico" type="image/x-icon">
+            <meta name="description" content="Écoutez les meilleurs sons de rap et découvrez des paroles percutantes.">
+            <meta name="keywords" content="raous, zbig, voleur2voleur, taftaf, rap, musique">
+            <meta property="og:title" content="Rap Attack Player">
+            <meta property="og:description" content="Écoutez les meilleurs sons de rap et découvrez des paroles percutantes.">
+            <meta property="og:image" content="rap.jpg">
+            <meta property="og:url" content="http://trkn-player.jeankilindrangh.repl.co:${port}/">
+            <meta property="og:type" content="website">
+            <link rel="canonical" href="http://trkn-player.jeankilindrangh.repl.co:${port}/"> <!-- Ajoutez une balise de lien canonique -->
+            <style>
+               body {
+                    background-color: #222; /* Gris foncé */
+                    color: #fff; /* Blanc */
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                }
+                header {
+                    background-color: #ff5858; /* Rouge */
+                    padding: 10px 0;
+                    text-align: center;
+                }
+                h1 {
+                    margin: 0;
+                    font-size: 28px;
+                }
+                nav {
+                    text-align: center;
+                    margin-top: 10px;
+                }
+                nav a {
+                    color: #ff5858; /* Rouge */
+                    text-decoration: none;
+                    margin: 0 15px;
+                }
+
+                .rap h2 {
+                    color: #ff5858; /* Rouge */
+                    margin-top: 0;
+                }
+                .rap p {
+                    margin: 0;
+                }
+                video {
+                    outline: 2px solid #00ff00; /* Vert */
+                }
+
+                /* Ajout des animations */
+                .rap-image {
+                    display: block;
+                    margin: auto;
+                    max-width: 100%;
+                    max-height: 300px;
+                    animation: fadeIn 1s ease-in-out;
+                }
+                .rap {
+                     background-color: #00e676; /* Vert fluo */
+                    padding: 20px;
+                    margin: 10px;
+                    border: 2px solid #ffeb3b; /* Jaune fluo */
+                    border-radius: 10px;
+                    animation: scaleIn 1s ease-in-out;
+                }
+                @keyframes fadeIn {
+                    from { opacity: 0; }
+                    to { opacity: 1; }
+                }
+                @keyframes scaleIn {
+                    from { transform: scale(0.5); opacity: 0; }
+                    to { transform: scale(1); opacity: 1; }
+                }
+            </style>
+        </head>
+        <body>      <header>
+                <h1>Rap Attack Player</h1>
+                <nav>
+                    <a href="#accueil"><font color=black>Accueil</a></font>
+                    <a href="#sons"><font color=black>Sons</a></font>
+                   <a href="#apropos"><font color=black> À propos</a></font>
+                </nav>
+            </header>
+            <!-- Afficher l'image avec animation de préchargement -->
+            <img src="rap.jpg" alt="Image de Rap" class="rap-image" onload="this.style.opacity='1'">
+            <main>
+                <section id="accueil">
+                    <h2>Accueil</h2>
+                    <!-- Ajoutez du contenu pour la section Accueil si nécessaire -->
+                </section>
+                <section id="sons">
+                    <h2>C'est du lourd !</h2>
+                    ${rapContentHTML}
+
+                    <h2>Fais péter les sons</h2>
+                    <ul>${listHTML}</ul>
+                </section>
+                <section id="apropos">
+                    <h2>À propos</h2>
+                    <!-- Ajoutez du contenu pour la section À propos si nécessaire -->
+                </section>
+            </main>
+                 <footer>
+            <center><p>© ${new Date().getFullYear()} - Trkn Rap Attack Player</p></center>
+        </footer>
+            <script>
+                // Précharger les vidéos
+                const videos = document.querySelectorAll('video');
+                videos.forEach(video => video.preload = 'auto');
+            </script>
+        </body>
+        </html>
+    `;
+}
+
+app.get('/play', (req, res) => {
+    const file = sanitizeInput(req.query.file); // Filtrer les entrées utilisateur
+    const filePath = path.join(__dirname, 'public', file);
+    const fileType = path.extname(file).toLowerCase();
+
+    if (['.mp3', '.webm', '.mp4'].includes(fileType)) {
+        res.sendFile(filePath);
+    } else {
+        res.status(404).send('File not found');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
